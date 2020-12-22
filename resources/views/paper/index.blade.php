5c203f4a0ddea680f02585082dcbed44aa350de8
Aliux <aliux02@gmail.com>
2020-12-22 23:26:02 +0200

    pavadinimas

--- resources/views/paper/index.blade.php
+++ resources/views/paper/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Popieriaus sandelys</title>
+    <title>Popieriaus sandelis</title>
     <style>
         table {
           font-family: arial, sans-serif;
